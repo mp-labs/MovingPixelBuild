3608ef42722990d8b6ff5c07e75a7d4867cb7bee
Romuald <romuald@mp-labs.net>
2013-07-09 09:50:53 +0200

    Adding function to parse version string.

--- source/mpbCommon.cmake
+++ source/mpbCommon.cmake
@@ -19,6 +19,43 @@ function( mpb_display_title text )
     mpb_display_title_bar( 80 "#" )
 endfunction( mpb_display_title text )
 
+# Parse a version string and returns it as a list
+function( mpb_parse_version_string version output )
+    set( ${output} )
+    set( version_string ${version} )
+    string( FIND ${version_string} "." idx )
+    while( idx GREATER -1 )
+        # Take the first part
+        string( SUBSTRING ${version} 0 ${idx} part )
+        # Store it in the result
+        set( ${output} ${${output}} ${part} )
+        # Increment the index
+        math( EXPR idx  "${idx} + 1" )
+        # Remote the beginning of the string
+        string( SUBSTRING ${version_string} ${idx} -1 version_string )
+        # Repeat the search
+        string( FIND ${version_string} "." idx )
+    endwhile( idx GREATER -1 )
+
+    string( FIND ${version_string} " " idx )
+    while( idx GREATER -1 )
+        # Take the first part
+        string( SUBSTRING ${version_string} 0 ${idx} part )
+        # Store it in the result
+        set( ${output} ${${output}} ${part} )
+        # Increment the index
+        math( EXPR idx  "${idx} + 1" )
+        # Remote the beginning of the string
+        string( SUBSTRING ${version_string} ${idx} -1 version_string )
+        # Repeat the search
+        string( FIND ${version_string} " " idx )
+    endwhile( idx GREATER -1 )
+
+    set( ${output} ${${output}} ${version_string} )
+    set( ${output} ${${output}} PARENT_SCOPE )
+
+endfunction( mpb_parse_version_string version output_variable )
+
 mpb_display_title( "Moving Pixel Build (MPB) ${MPB_VERSION_STRING}" )
 message( "CMake:                    ${CMAKE_VERSION}" )
 message( "Project Root Directory:   ${PROJECT_SOURCE_DIR}" )
