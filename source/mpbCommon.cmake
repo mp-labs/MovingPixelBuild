08059c13b2d0470f0bd04a9862e4e1eba1ef0468
Romuald <romuald@mp-labs.net>
2013-07-09 09:51:17 +0200

    Adding some documentation.

--- source/mpbCommon.cmake
+++ source/mpbCommon.cmake
@@ -1,15 +1,18 @@
 
 include( mpbVersion )
 
+# Stop configuration and display error message
 function( mpb_error_fatal message )
     message( FATAL_ERROR ${message} )
 endfunction( mpb_error_fatal message )
 
-function( mpb_display_title_bar size char )
-    string( RANDOM LENGTH ${size} ALPHABET ${char} result )
+# Display a bar of the specified width with the specified character
+function( mpb_display_title_bar width char )
+    string( RANDOM LENGTH ${width} ALPHABET ${char} result )
     message( ${result} )
 endfunction( mpb_display_title_bar size char )
 
+# Display a pretty title text
 function( mpb_display_title text )
     string( LENGTH ${text} text_len )
     math( EXPR text_len "(80 - ${text_len}) / 2" )
@@ -56,6 +59,7 @@ function( mpb_parse_version_string version output )
 
 endfunction( mpb_parse_version_string version output_variable )
 
+# Display introduction banner for Moving Pixel Build
 mpb_display_title( "Moving Pixel Build (MPB) ${MPB_VERSION_STRING}" )
 message( "CMake:                    ${CMAKE_VERSION}" )
 message( "Project Root Directory:   ${PROJECT_SOURCE_DIR}" )
